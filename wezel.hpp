2b43b3f5f24c0902652754b4031b1c07bfd585d7
Michalowski Wojciech <wmichalo@Linux.labnat.iiar.pwr.edu.pl>
2016-05-25 19:07:53 +0200

    0.

---
+++ wezel.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum gracz {KOMPUTER, CZLOWIEK};
+
+class wezel {
+
+  int waga;
+  gracz Gracz;
+
+public:
+
+  wezel();
+
+  
+
+};
